8c74b36b4a008893f0a680d23c27878eaf58262b
thejasonminh <123194102+thejasonminh@users.noreply.github.com>
2023-11-25 11:41:40 -0500

    init main file

---
+++ crittercraft.py
@@ -0,0 +1,7 @@
+# Author: Jason Ngo, Jayden Jung, Jueun Kang
+# Class: INF 0452H; Information Design Studio V: Coding
+# Instructor: Maher Elshakankiri
+# Assignment: Final Project
+# Date Created: 11/25/2023
+# Last Modified: 11/25/2023
+# Purpose: Crittercraft
