4f1dc7f2cf917a42b8e6d2492ed67853395dcc79
jueunkang12 <jueun.kang@mail.utoronto.ca>
2023-12-01 19:16:12 -0500

    Tictacbone - game working, result display is not

--- play.py
+++ play.py
@@ -1,99 +1,98 @@
 import tkinter as tk
 from tkinter import messagebox
 import random
+import time
 
 
-class TicTacToeComputerPlayer:
+class TicTacBoneGUI:
     def __init__(self):
         self.window = tk.Tk()
-        self.window.title("Tic-Tac-Toe - Bone Theme")
+        self.window.title("Tic-Tac-Bone")
 
-        self.current_player = "X"
+        self.players_symbol = {"X": "\U0001F984", "O": "\U0001F43E"}
+        self.user = "X"
         self.board = [["", "", ""], ["", "", ""], ["", "", ""]]
-
         self.buttons = [[None, None, None], [
             None, None, None], [None, None, None]]
+        self.current_player = "X"
 
         for i in range(3):
             for j in range(3):
-                self.buttons[i][j] = tk.Button(self.window, text="", font=("Helvetica", 24),
-                                               width=5, height=2, command=lambda row=i, col=j: self.on_click(row, col))
+                self.buttons[i][j] = Button(
+                    self.window, text="", font=("Arial", 24),
+                    width=5, height=2, command=lambda row=i, column=j: self.on_click(row, column)
+                )
                 self.buttons[i][j].grid(row=i, column=j)
 
-        self.computer_player = "O"
-        self.user_can_click = True  # Flag to control user input
+        self.computer = "O"
+        self.user_turn = True
 
-    def on_click(self, row, col):
-        if self.user_can_click and self.board[row][col] == "" and not self.check_winner():
-            self.user_can_click = False  # Disable user input during computer's turn
-            self.board[row][col] = self.current_player
-            self.update_button(row, col)
+        self.window.mainloop()
 
-            if self.check_winner():
-                messagebox.showinfo(
-                    "Tic-Tac-Toe", f"Player {self.current_player} wins!")
-                self.reset_game()
-            elif self.check_tie():
-                messagebox.showinfo("Tic-Tac-Toe", "It's a tie!")
-                self.reset_game()
+    def on_click(self, row, column):
+        if self.user_turn and self.board[row][column] == "" and not self.winner_check():
+            self.user_turn = False
+            self.board[row][column] = self.current_player
+            self.button_update(row, column)
+
+            if self.winner_check():
+                tkinter.messagebox.showinfo("You earn 1 \U0001F984!")
+                self.game_reset()
+            elif self.tie_check():
+                tkinter.messagebox.showinfo(
+                    "It's a tie! Play again to earn \U0001F984")
+                self.game_reset()
             else:
-                self.current_player = "O" if self.current_player == "X" else "X"
+                self.window.after(1000, self.computer_turn)
 
-                if self.current_player == self.computer_player:
-                    # After a delay, enable user input and let the computer make its move
-                    self.window.after(1000, self.computer_move)
+    def button_update(self, row, column):
+        symbol = self.players_symbol.get(self.current_player, "")
+        self.buttons[row][column].config(text=symbol, state=DISABLED)
 
-    def update_button(self, row, col):
-        symbol = "🦴" if self.current_player == "X" else "🐾"
-        self.buttons[row][col].config(text=symbol, state=tk.DISABLED)
-
-    def check_winner(self):
+    def winner_check(self):
         for i in range(3):
             if self.board[i][0] == self.board[i][1] == self.board[i][2] != "":
-                return True  # Check rows
+                return True
             if self.board[0][i] == self.board[1][i] == self.board[2][i] != "":
-                return True  # Check columns
+                return True
 
         if self.board[0][0] == self.board[1][1] == self.board[2][2] != "":
-            return True  # Check diagonal from top-left to bottom-right
+            return True
         if self.board[0][2] == self.board[1][1] == self.board[2][0] != "":
-            return True  # Check diagonal from top-right to bottom-left
+            return True
 
         return False
 
-    def check_tie(self):
+    def tie_check(self):
         for i in range(3):
             for j in range(3):
                 if self.board[i][j] == "":
                     return False
         return True
 
-    def reset_game(self):
-        for i in range(3):
-            for j in range(3):
-                self.board[i][j] = ""
-                self.buttons[i][j].config(text="", state=tk.NORMAL)
-        self.user_can_click = True  # Reset user input flag
+    def game_reset(self):
+        self.window.destroy()
+        start_new_game = TicTacBoneGUI()
 
-    def computer_move(self):
-        empty_cells = [(i, j) for i in range(3)
-                       for j in range(3) if self.board[i][j] == ""]
-        if empty_cells:
-            row, col = random.choice(empty_cells)
-            self.board[row][col] = self.computer_player
-            self.update_button(row, col)
-            if self.check_winner():
-                messagebox.showinfo(
-                    "Tic-Tac-Toe", f"Player {self.computer_player} wins!")
-                self.reset_game()
-            elif self.check_tie():
-                messagebox.showinfo("Tic-Tac-Toe", "It's a tie!")
-                self.reset_game()
+    def computer_turn(self):
+        empty_slot = [(i, j) for i in range(3)
+                      for j in range(3) if self.board[i][j] == ""]
+        if empty_slot:
+            row, column = random.choice(empty_slot)
+            self.board[row][column] = "O"
+            self.button_update(row, column)
+
+            if self.winner_check():
+                tkinter.messagebox.showinfo(
+                    "You lost! Play again to earn \U0001F984")
+                self.game_reset()
+            elif self.tie_check():
+                tkinter.messagebox.showinfo(
+                    "It's a tie! Play again to earn \U0001F984")
+                self.game_reset()
             else:
                 self.current_player = "X"
-                self.user_can_click = True  # Enable user input for the next turn
+                self.user_turn = True
 
 
-if __name__ == "__main__":
-    game = TicTacToeComputerPlayer()
-    game.window.mainloop()
+TicTacBoneGUI()
